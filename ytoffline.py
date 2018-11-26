cddaf8ac29f860b551c6604c354ccaa7269d14c3
m-we <mwea@protonmail.com>
2018-11-26 07:25:16 -0500

    Update

--- ytoffline.py
+++ ytoffline.py
@@ -1,48 +1,36 @@
-# m-we
-# v1.0.3 (2018-04-13)
-# ytoffline - YouTube subscriptions plain and simple, no algorithms. Also
-#             useful for those looking for privacy who don't want their
-#             subscriptions associated with a Google account.
-#
-#             To add a channel to subs.json, use the channel ID. This is often
-#             in the URL (youtube.com/channel/UC123...ABC). If it isn't, go to
-#             the channel page, view the page source, and search for
-#             channel_id= using CTRL+F.
-#
-# Requires feedparser.
-
 import datetime
-import feedparser
 import json
 
-feedb = "https://www.youtube.com/feeds/videos.xml?channel_id="
+import feedparser
+
+feedb = 'https://www.youtube.com/feeds/videos.xml?channel_id='
 # Set days= to whatever number you want if a week is too much or too little.
 # Feedparser returns a limited number of videos, however, so setting days to
 # 10000 probably won't return every video a channel has ever uploaded.
 db = datetime.datetime.now() - datetime.timedelta(days=7)
-bench = int(str(db)[0:10].replace("-", ""))
+bench = int(str(db)[0:10].replace('-', ''))
 
 # Does everything.
 def Subs():
     urls = []
     # See example subs.json for formatting. Fairly simple.
-    with open("subs.json", "r") as fp:
+    with open('subs.json', 'r') as fp:
         subs = json.load(fp)
 
     vids = []
     for sub in subs:
         # Print all of the initial stuff on the same line to save space.
-        print("Retrieving videos from " + sub + ".", end="\r")
+        print('Retrieving videos from ' + sub, end='\r')
         feed = feedparser.parse(feedb + subs[sub])
-        for j in feed["items"]:
+        for j in feed['items']:
             # Get int of publication date and verify it's within the past week.
-            draw = j["published_parsed"]
+            draw = j['published_parsed']
             d = draw.tm_year * 10000 + draw.tm_mon * 100 + draw.tm_mday
             if d >= bench:
                 # Append data to a list to be sorted by publication date.
                 dat = [
-                    j["published_parsed"], sub, j["link"],
-                    j["media_thumbnail"][0]["url"], j["title"]
+                    j['published_parsed'], sub, j['link'],
+                    j['media_thumbnail'][0]['url'], j['title']
                 ]
                 vids.append(dat)
 
@@ -51,9 +39,9 @@ def Subs():
     # from the command line.
     vids = sorted(vids)
     for v in vids:
-        d = str(v[0].tm_year) + "-" + str(v[0].tm_mon) + "-" + str(v[0].tm_mday)
-        print(v[1] + " - " + v[4] + " - " + d + " - " + v[2])
+        d = str(v[0].tm_year) + '-' + str(v[0].tm_mon) + '-' + str(v[0].tm_mday)
+        print(v[1] + ' - ' + v[4] + ' - ' + d + ' - ' + v[2])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     Subs()
-    input("\n\nPress enter to exit.")
+    input('\n\nPress enter to exit.')
